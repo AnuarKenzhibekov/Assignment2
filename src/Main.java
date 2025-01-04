92852dcea447b0cdf6c04982e78c239934e4e4c8
AnuarKenzhibekov <kenzhibekovanuar@mail.ru>
2025-01-04 11:36:10 +0600

    dwladkadfs

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,20 @@ import java.util.List;
 public class Main{
 
     public static void main(String[] args) {
-        Admin admin1 = new Admin(1, "Admin1", "admin1@example.com");
-        Admin admin2 = new Admin(2, "Admin2", "admin2@example.com");
+        Admin first_admin = new Admin(1, "Admin1", "admin1@example.com");
+        Admin second_admin = new Admin(2, "Admin2", "admin2@example.com");
 
         Product p1 = new Product("P001", "PC", 1000.00, 10);
         Product p2 = new Product("P002", "phone", 500.00, 15);
 
-        admin1.addProduct(p1);
-        admin1.addProduct(p2);
+        first_admin.addProduct(p1);
+        second_admin.addProduct(p2);
 
         Customer c1 = new Customer(1, "Customer1", "customer1@example.com", "Address1");
         Customer c2 = new Customer(2, "Customer2", "customer2@example.com", "Address2");
 
         c1.placeOrder(p1, 2);
         c2.placeOrder(p2, 5);
-
-        c2.cancelOrder();
 
         System.out.println("\n--- All Products ---");
         p1.displayDetails();
